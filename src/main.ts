247c0e6e6c721c06997531297af4f4689e7eb24d
Wa-dot <842G1thub@$52>
2023-11-06 11:14:56 +0100

    maj i18n config (used depreceated version)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import i18n from "./i18n"
+import { createI18n } from 'vue-i18n';
+import en from './i18n/en.json'
+import fr from './i18n/fr.json'
 
 
+const i18n =  createI18n({
+    locale: import.meta.env.VITE_DEFAULT_LOCALE,
+    fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE,
+    legacy: false,
+    globalInjection: true,
+    messages: {
+        fr,
+        en
+    }
+})
 
 createApp(App).
     use(router).
